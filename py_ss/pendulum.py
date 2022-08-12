4e6a6f8a0ec8a48c3a51e5a7dbe65c184c55c01c
Moji Fathi <moji.fathi@torc.ai>
2022-08-12 15:21:12 -0400

    add simple pendulum

---
+++ py_ss/pendulum.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed Aug 10 20:52:40 2022
+
+@author: fathi
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import blocks
+import solver
+
+class SSModel(blocks.MainModel):
+    def __init__(self):
+        blocks.MainModel.__init__(self, 'pendulum')
+
+        self._dphi    = blocks.Integrator(np.pi/4, 'dphi', 'd2phi', 'dphi')
+        self._phi     = blocks.Integrator(0.0, 'phi', 'dphi', 'phi')
+        self._sin_phi = blocks.Sin('sin(phi)', 'phi', 'sin_phi')
+        self._gain    = blocks.Gain(-9.81/0.1, '-g/l', 'sin_phi', 'd2phi')
+
+        self.add_component(self._dphi)
+        self.add_component(self._phi)
+        self.add_component(self._sin_phi)
+        self.add_component(self._gain)
+
+if __name__ == '__main__':
+    model = SSModel()
+    states = []
+    model.get_states(states)
+    
+    state_names = [state._state for state in states]
+
+    def solver_callback(t, x):
+        x = {k: v for k, v in zip(state_names, x)}
+        model.process(t, x)
+        return np.array([x[state._deriv] for state in states])
+
+    h = 0.01    
+    x = np.array([state._value for state in states])
+    t = 0.0
+    X = [x]
+    T = [t]
+    k = 0
+    while t < 5.0:
+        x = solver.rk4(solver_callback, t0=t, x0=x, h=h)
+        k += 1
+        t = k*h
+        print(k, t)
+
+        model.update_states(t, {k: v for k, v in zip(state_names, x)})
+
+        X.append(x)
+        T.append(t)
+
+    plt.plot(T, [x[1] for x in X])
+    plt.show()

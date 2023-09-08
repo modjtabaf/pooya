7c0a42bf3bf4b040dd294232f0ffa2d1d1480773
mojtaba <mojtaba.fathi@gmail.com>
2023-09-08 14:00:48 -0400

    update build scripts

--- cpp_ss/sandbox/build.sh
+++ cpp_ss/sandbox/build.sh
@@ -1,2 +1,3 @@
 
-make -f $1.Makefile && ./build/apps/$1
+make -f $1.Makefile "${@:2}"
+./build/apps/$1
--- cpp_ss/sandbox/rebuild.sh
+++ cpp_ss/sandbox/rebuild.sh
@@ -1,2 +1,4 @@
 
-make clean -f $1.Makefile && make -f $1.Makefile && ./build/apps/$1
+make clean -f $1.Makefile
+make -f $1.Makefile "${@:2}"
+./build/apps/$1

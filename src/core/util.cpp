35bfa3755ed657b6823adb7beaebce9719da4cbf
Moji Fathi <moji.fathi@torc.ai>
2024-01-26 13:37:03 -0500

    improve the verify macro

--- src/core/util.cpp
+++ src/core/util.cpp
@@ -5,7 +5,27 @@ namespace pooya::util
 {
 
 #ifndef NDEBUG
+
 std::vector<PooyaTraceInfo> pooya_trace_info;
-#endif
+
+void pooya_throw_exception(const std::string& file, int line, const std::string& msg)
+{
+    std::stringstream full_msg;
+    full_msg <<
+        "\nPooya Exception:\n" <<
+        "  " << file << ":" << std::to_string(__LINE__) << "\n"
+        "  " << msg << "\n\n";
+    full_msg << "Pooya Traceback:\n";
+    for (auto it=pooya::util::pooya_trace_info.rbegin(); it != pooya::util::pooya_trace_info.rend(); it++)
+    {
+        full_msg <<
+            "- " + it->_file << ":" << std::to_string(it->_line) << "\n";
+        if (!it->_msg.empty())
+            full_msg << "  " << it->_msg << "\n";
+    }
+    throw std::runtime_error(full_msg.str());
+}
+
+#endif // !defined(NDEBUG)
 
 }
--- src/core/util.hpp
+++ src/core/util.hpp
@@ -35,24 +35,10 @@ namespace pooya::util
 #define verify(cond, msg)
 #define pooya_trace(msg)
 #else
-#define verify(cond, msg) \
-    if (!(cond)) \
-    { \
-        std::stringstream full_msg; \
-        full_msg << \
-            "\nPooya Exception:\n" << \
-            std::string("  " __FILE__ ":") << std::to_string(__LINE__) << "\n" \
-            "  " << (msg) << "\n\n"; \
-        full_msg << "Pooya Traceback:\n"; \
-        for (auto it=pooya::util::pooya_trace_info.rbegin(); it != pooya::util::pooya_trace_info.rend(); it++) \
-        { \
-            full_msg << \
-                "- " + it->_file << ":" << std::to_string(it->_line) << "\n"; \
-            if (!it->_msg.empty()) \
-                full_msg << "  " << it->_msg << "\n"; \
-        } \
-        throw std::runtime_error(full_msg.str()); \
-    }
+
+void pooya_throw_exception(const std::string& file, int line, const std::string& msg);
+
+#define verify(cond, msg) if (!(cond)) {pooya::util::pooya_throw_exception(__FILE__, __LINE__, msg);}
 
 struct PooyaTraceInfo
 {

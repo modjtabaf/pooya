94dcbc0bb266039bb8d17006f2e6624c91577250
Mojtaba Fathi <mojtaba.fathi@gmail.com>
2024-02-08 22:25:49 -0500

    delay and memory block tests and samples

---
+++ tests/test_delay.cpp
@@ -0,0 +1,140 @@
+/*
+Copyright 2023 Mojtaba (Moji) Fathi
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <cstddef>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <chrono>
+
+#include <gtest/gtest.h>
+
+#include "src/core/pooya.hpp"
+#include "src/core/helper.hpp"
+#include "src/core/solver.hpp"
+#include "src/misc/gp-ios.hpp"
+
+class TestDelay : public testing::Test
+{
+public:
+    TestDelay()
+    {
+        //
+    }
+};
+
+TEST_F(TestDelay, ScalarDelay)
+{
+    // test parameters
+    double time_delay = 2.78;
+    double t_end = 7.32;
+    double dt = 0.01;
+    double initial = 0.0;
+    auto func = [](double t) -> double
+    {
+        return -1.3*t + 4.92;
+    };
+
+    // model setup
+    pooya::Model model;
+    pooya::Delay delay("delay");
+    pooya::ScalarSignalId s_time_delay = model.create_scalar_signal("time_delay");
+    pooya::ScalarSignalId s_initial = model.create_scalar_signal("initial");
+    pooya::ScalarSignalId s_x = model.create_scalar_signal("x");
+    pooya::ScalarSignalId s_y = model.create_scalar_signal("y");
+    model.add_block(delay, {
+        {"delay", s_time_delay},
+        {"in", s_x},
+        {"initial", s_initial}}, s_y);
+
+    // simulator setup
+    pooya::Simulator sim(model,
+        [&](pooya::Model&, double t, pooya::Values& values) -> void
+        {
+            values.set(s_time_delay, time_delay);
+            values.set(s_initial, initial);
+            values.set(s_x, func(t));
+        });
+
+    double t;
+
+    // initialize and run the simulation
+    sim.init(0.0);
+    for (t = dt; t < time_delay/2; t += dt)
+        sim.run(t);
+
+    // verify the results
+    EXPECT_EQ(initial, sim.values().get(s_y));
+
+    // continue running the simulation
+    for (; t < t_end; t += dt)
+        sim.run(t);
+
+    // verify the results
+    EXPECT_NEAR(func(t_end - time_delay), sim.values().get(s_y), 1e-10);
+}
+
+TEST_F(TestDelay, ArrayDelay)
+{
+    // test parameters
+    constexpr std::size_t N = 5;
+    double time_delay = 2.78;
+    double t_end = 7.32;
+    double dt = 0.01;
+    pooya::ArrayN<N> initial = pooya::ArrayN<N>::Zero();
+    auto func = [](double t) -> pooya::ArrayN<N>
+    {
+        static const pooya::ArrayN<N> A{3.7, -2.5, 10.45, 0.0, -0.02};
+        static const pooya::ArrayN<N> B{-10.56, 0.18, 7.24, -3.67, 0.876};
+        return A*t + B;
+    };
+
+    // model setup
+    pooya::Model model;
+    pooya::DelayA delay("delay");
+    pooya::ScalarSignalId s_time_delay = model.create_scalar_signal("time_delay");
+    pooya::ArraySignalId s_initial = model.create_array_signal("initial", N);
+    pooya::ArraySignalId s_x = model.create_array_signal("x", N);
+    pooya::ArraySignalId s_y = model.create_array_signal("y", N);
+    model.add_block(delay, {
+        {"delay", s_time_delay},
+        {"in", s_x},
+        {"initial", s_initial}}, s_y);
+
+    // simulator setup
+    pooya::Simulator sim(model,
+        [&](pooya::Model&, double t, pooya::Values& values) -> void
+        {
+            values.set(s_time_delay, time_delay);
+            values.set(s_initial, initial);
+            values.set(s_x, func(t));
+        });
+
+    double t;
+
+    // initialize and run the simulation
+    sim.init(0.0);
+    for (t = dt; t < time_delay/2; t += dt)
+        sim.run(t);
+
+    // verify the results
+    EXPECT_EQ((initial - sim.values().get(s_y)).abs().maxCoeff(), 0);
+
+    // continue running the simulation
+    for (; t < t_end; t += dt)
+        sim.run(t);
+
+    // verify the results
+    EXPECT_NEAR((func(t_end - time_delay) - sim.values().get(s_y)).abs().maxCoeff(), 0, 1e-10);
+}
---
+++ tests/test_memory.cpp
@@ -0,0 +1,94 @@
+/*
+Copyright 2023 Mojtaba (Moji) Fathi
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <cstddef>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <chrono>
+
+#include <gtest/gtest.h>
+
+#include "src/core/pooya.hpp"
+#include "src/core/helper.hpp"
+#include "src/core/solver.hpp"
+#include "src/misc/gp-ios.hpp"
+
+class TestMemory : public testing::Test
+{
+public:
+    TestMemory()
+    {
+        //
+    }
+};
+
+TEST_F(TestMemory, ScalarMemory)
+{
+    // test parameters
+    double x = 3.7;
+    double x0 = -4.8;
+
+    // model setup
+    pooya::Model model;
+    pooya::Memory memory("memory", x0);
+    pooya::ScalarSignalId s_x = model.create_scalar_signal("x");
+    pooya::ScalarSignalId s_y = model.create_scalar_signal("y");
+    model.add_block(memory, s_x, s_y);
+
+    // simulator setup
+    pooya::Simulator sim(model,
+        [&](pooya::Model&, double /*t*/, pooya::Values& values) -> void
+        {
+            values.set(s_x, x);
+        });
+
+    // do one step
+    sim.init(0.0);
+
+    // verify the results
+    EXPECT_EQ(x0, sim.values().get(s_y));
+}
+
+TEST_F(TestMemory, ArrayMemory)
+{
+    // test parameters
+    constexpr std::size_t N = 4;
+    pooya::ArrayN<N> x{3.7, -2.5, 10.45, 0.0};
+    pooya::ArrayN<N> x0{-10.56, 0.18, 7.24, -3.67};
+
+    // model setup
+    pooya::Model model;
+    pooya::MemoryA memory("memory", x0);
+    pooya::ArraySignalId s_x = model.create_array_signal("x", N);
+    pooya::ArraySignalId s_y = model.create_array_signal("y", N);
+    model.add_block(memory, s_x, s_y);
+
+    // simulator setup
+    pooya::Simulator sim(model,
+        [&](pooya::Model&, double /*t*/, pooya::Values& values) -> void
+        {
+            values.set(s_x, x);
+        });
+
+    // do one step
+    sim.init(0.0);
+
+    // verify the results
+    auto y = sim.values().get(s_y);
+    for (std::size_t k=0; k < N; k++)
+    {
+        EXPECT_EQ(y[k], x0[k]);
+    }
+}

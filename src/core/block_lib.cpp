410e8d2c8ab1ccbd8dc9cf26114390649344afbe
Moji Fathi <moji.fathi@torc.ai>
2024-04-08 09:10:04 -0400

    fix a bug in initial boolean value of a bus memory

--- src/core/block_lib.cpp
+++ src/core/block_lib.cpp
@@ -103,7 +103,7 @@ void BusMemory::block_builder(const std::string &full_label, const BusSpec::Wire
     {
         block = (it == _init_values.end())
             ? std::make_unique<MemoryB>("memory")
-            : std::make_unique<MemoryB>("memory", it->second.as_scalar() == 0);
+            : std::make_unique<MemoryB>("memory", it->second.as_scalar() != 0);
     }
     else if (wi._array_size > 0)
     {

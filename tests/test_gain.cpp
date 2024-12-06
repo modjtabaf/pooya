f35a2261ef63a66fcda1be4d27a3d70a46216ed6
Chandramouli Gnanasambandham <chandramouli.gnanasambandham@itm.uni-stuttgart.de>
2024-12-06 15:27:20 +0100

    Write a test to extend Gain Block test for int (#58)
    * write a parameterized typed test to extend testing for int, and float types
    
    * add a test for int gain
    
    ---------
    
    Co-authored-by: Chandramouli Gnanasambandham <chandramouli@torc.ai>

--- tests/test_gain.cpp
+++ tests/test_gain.cpp
@@ -1,18 +1,22 @@
 /*
 Copyright 2024 Mojtaba (Moji) Fathi
 
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-persons to whom the Software is furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include <cstddef>
@@ -53,7 +57,29 @@ TEST_F(TestGain, ScalarGain)
     sim.init(0.0);
 
     // verify the results
-    EXPECT_NEAR(gain_value * x, s_y, 1e-10);
+    EXPECT_DOUBLE_EQ(gain_value * x, s_y);
+}
+
+TEST_F(TestGain, IntGain)
+{
+    // test parameters
+    const int x = 3;
+    constexpr int gain_value{2};
+
+    // model setup
+    pooya::GainT<int, int> gain(gain_value);
+    pooya::IntSignal s_x;
+    pooya::IntSignal s_y;
+    gain.connect(s_x, s_y);
+
+    // simulator setup
+    pooya::Simulator sim(gain, [&](pooya::Block&, double /*t*/) -> void { s_x = x; });
+
+    // do one step
+    sim.init(0.0);
+
+    // verify the results
+    EXPECT_EQ(gain_value * x, s_y);
 }
 
 TEST_F(TestGain, ArrayGain)
